b6f116bd36cc8279d0806490f09ecdfef655c3ed
MarcosIraola <iraolamarcos@gmail.com>
2022-10-16 03:16:46 -0300

    update

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import Popup from './components/Popup.vue'
+</script>
+
+<template>
+
+  <h1>Hola</h1>
+  <Teleport to="#portal-target">
+    <Popup ref="popup" :title="'Pop up test'" :sub="'Marcos blabla'">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus repellat itaque totam explicabo odit nemo voluptatum, non magnam eos voluptatem cum voluptatibus qui nulla fugiat dolor vero, dolorum amet at.
+    </Popup>
+  </Teleport>
+  <button class="btn" @click="$refs.popup.show = !$refs.popup.show">
+    Open Modal
+  </button>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Popup.vue
@@ -0,0 +1,80 @@
+<template>
+    <div >
+      <div id="open-modal" :class="{ show, 'modal-window': true }">
+        <div class="modal">
+          <a @click="show = false" title="Close" class="modal-close">Close</a>
+          <h1>{{ title }}</h1>
+          <div>
+            <slot></slot>
+          </div>
+          <div>
+            <small>{{ sub }}</small>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ["title", "sub"],
+    data() {
+      return {
+        show: false
+      };
+    }
+
+  };
+  </script>
+  
+  <style scoped>
+  .modal-window div:not(:last-of-type) {
+    margin-bottom: 15px;
+  }
+  .modal-window {
+    background-color: rgb(134, 134, 134);
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 10000;
+    opacity: 0;
+    transition: all 0.3s;
+  }
+
+  div {
+      width: 400px;
+      height: 300px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      padding: 2em;
+    }
+
+  .show {
+    visibility: visible;
+    opacity: 1;
+    pointer-events: auto;
+  }
+
+  .modal {
+    display: flex;
+    flex-direction: column;
+  }
+  .modal-close {
+    color: #aaa;
+    line-height: 50px;
+    font-size: 80%;
+    position: absolute;
+    right: 0;
+    text-align: center;
+    top: 0;
+    width: 70px;
+    text-decoration: none;
+    z-index: 1000;
+    cursor: pointer;
+  }
+  .modal-close:hover {
+    color: black;
+  }
+  </style>
